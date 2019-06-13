3d38b322cd60859d9cc1acb8c15ddd2c0415e71a
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-13 23:54:38 +0530

    Created a basic react-cli

---
+++ index.js
@@ -0,0 +1,80 @@
+const inquirer = require("inquirer");
+const cmd = require("node-cmd");
+
+const cliCommand = [
+  {
+    type: "list",
+    name: "installation",
+    message: "What would you like to install?",
+    choices: [
+      "create-react-app",
+      "react-component",
+      "react-router",
+      "node-sass"
+    ]
+  }
+];
+
+const installOption = [
+  {
+    type: "list",
+    name: "decision",
+    message: "Would you like to Install or Uninstall?",
+    choices: ["Install", "Uninstall"]
+  }
+];
+
+const reactComponents = [
+  {
+    type: "list",
+    name: "component",
+    message: "Function or Class Component?",
+    choices: ["function", "class"]
+  }
+];
+
+const prompt = inquirer.createPromptModule();
+
+prompt(cliCommand).then(answers => {
+  switch (answers.installation) {
+    case "create-react-app":
+      cmd.run("npm i -g create-react-app");
+      break;
+    case "react-component":
+      prompt(reactComponents).then(answer => console.log(answer));
+      break;
+    case "react-router":
+      prompt(installOption).then(({ decision }) => {
+        if (decision === "Install") {
+          cmd.get("npm i react-router react-router-dom", function(
+            err,
+            data,
+            stderr
+          ) {
+            console.log(data);
+          });
+        } else {
+          cmd.get("npm uninstall react-router react-router-dom", function(
+            err,
+            data,
+            stderr
+          ) {
+            console.log(data);
+          });
+        }
+      });
+      break;
+    case "node-sass":
+      prompt(installOption).then(({ decision }) => {
+        if (decision === "Install") {
+          cmd.get("npm i node-sass", function(err, data, stderr) {
+            console.log(data);
+          });
+        } else {
+          cmd.get("npm uninstall node-sass", function(err, data, stderr) {
+            console.log(data);
+          });
+        }
+      });
+  }
+});
