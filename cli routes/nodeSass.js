4022d2144475f79ccaa05fe5b344a770f7dd8782
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-14 11:08:14 +0530

    React-cli basic structure

--- cli routes/nodeSass.js
+++ cli routes/nodeSass.js
@@ -1,10 +1,11 @@
 const inquirer = require("inquirer");
 const cmd = require("node-cmd");
 
+//Cli Model
 const cliModel = require("../cli model/cli-model");
+const installOption = cliModel.installOption;
 
 const prompt = inquirer.createPromptModule();
-const installOption = cliModel.installOption;
 
 const nodeSass = () => {
   prompt(installOption).then(({ decision }) => {
