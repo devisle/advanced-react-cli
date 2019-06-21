0f7745f23ab356308c96c8f73343178b69ee6d28
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-21 21:11:33 +0530

    Adding in state management CLI options

--- cli routes/stateManagement.js
+++ cli routes/stateManagement.js
@@ -17,7 +17,52 @@ const stateManagement = () => {
       fs.mkdir("./store", err => {
         if (err) throw err;
       });
-      cmd.get(`cd store `);
+      fs.cmd.get(`cd store && touch store.js`);
+      const writeStream = fs.createWriteStream("./store/store.js");
+      writeStream.write(`import { createStore} from "redux";
+
+
+const initialState = {
+  // Declare your state here
+};
+
+// Your reducer
+const reducer = (state = initialState, action) => {
+  //Use Switch statements
+  switch(action.type){
+    case '':
+    //You usually perform a state change and return it
+    default:
+      return state;
+  }
+}
+
+// Add the below code within these comment blocks, to your respective files where you would like to have redux
+
+const mapStateToProps = state => {
+  return {
+    //Write your code here which connects the state of this component, to the Redux Store you have passed as props.
+  };
+};
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    //Write your code here which dispatches information to your reducer
+  };
+};
+
+
+//
+
+// Creating the store
+const store = createStore(reducer);
+
+//Exporting the Store
+export default store;
+
+
+      `);
     }
   });
 };
