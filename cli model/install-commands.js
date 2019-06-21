1611aa06a9ff277dfaa1015cbf9224379a2b58aa
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-21 19:54:31 +0530

    Adding in state management CLI options

--- cli model/install-commands.js
+++ cli model/install-commands.js
@@ -3,5 +3,7 @@ module.exports = {
   propTypes: "npm i prop-types",
   reactRouter: "npm i react-router react-router-dom",
   nodeSass: "npm i node-sass",
-  redux: "npm i redux react-redux"
+  redux: "npm i redux react-redux",
+  unstated: "yarn add unstated",
+  easyPeasy: "npm install easy-peasy"
 };
