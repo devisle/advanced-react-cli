44c55416243f236edd2d6a69bd830a7ec8a54899
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-21 19:39:26 +0530

    Adding install commands

--- cli model/install-commands.js
+++ cli model/install-commands.js
@@ -1,5 +1,7 @@
 module.exports = {
   cra: "npx create-react-app",
+  propTypes: "npm i prop-types",
   reactRouter: "npm i react-router react-router-dom",
-  nodeSass: "npm i node-sass"
+  nodeSass: "npm i node-sass",
+  redux: "npm i redux react-redux"
 };
