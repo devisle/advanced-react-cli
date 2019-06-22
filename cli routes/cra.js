25480cbb3040c40a847e867037077c831735e89a
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-22 09:30:49 +0530

    minor refactoring

--- cli routes/cra.js
+++ cli routes/cra.js
@@ -3,7 +3,6 @@ const cmd = require("node-cmd");
 
 // Cli Model
 const cliModel = require("../cli model/cli-model");
-const installOption = cliModel.installOption;
 const addPackage = cliModel.addPackage;
 const installFolder = cliModel.installFolder;
 
@@ -13,98 +12,92 @@ const installCommands = require("../cli model/install-commands");
 const prompt = inquirer.createPromptModule();
 
 const cra = () => {
-  prompt(installOption).then(({ decision }) => {
-    if (decision === "Install") {
-      prompt(installFolder).then(({ folderName }) => {
-        if (folderName !== "") {
-          console.log("React Router");
+  prompt(installFolder).then(({ folderName }) => {
+    if (folderName !== "") {
+      console.log("React Router");
+      prompt(addPackage).then(({ packageAdd }) => {
+        if (packageAdd === "y" || packageAdd === "Y") {
+          console.log("Node Sass");
           prompt(addPackage).then(({ packageAdd }) => {
             if (packageAdd === "y" || packageAdd === "Y") {
-              console.log("Node Sass");
-              prompt(addPackage).then(({ packageAdd }) => {
-                if (packageAdd === "y" || packageAdd === "Y") {
-                  cmd.get(
-                    ` mkdir ${folderName} && cd ${folderName} && ${
-                      installCommands.cra
-                    } . && ${installCommands.reactRouter} && ${
-                      installCommands.nodeSass
-                    }`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                } else if (packageAdd === "n" || packageAdd === "N") {
-                  cmd.get(
-                    ` mkdir ${folderName} && cd ${folderName} && ${
-                      installCommands.cra
-                    } . && ${installCommands.reactRouter}`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                }
-              });
+              cmd.get(
+                ` mkdir ${folderName} && cd ${folderName} && ${
+                  installCommands.cra
+                } . && ${installCommands.reactRouter} && ${
+                  installCommands.nodeSass
+                }`,
+                (err, data, stderr) => console.log(data)
+              );
             } else if (packageAdd === "n" || packageAdd === "N") {
-              console.log("Node Sass");
-              prompt(addPackage).then(({ packageAdd }) => {
-                if (packageAdd === "y" || packageAdd === "Y") {
-                  cmd.get(
-                    ` mkdir ${folderName} && cd ${folderName} && ${
-                      installCommands.cra
-                    } . && ${installCommands.nodeSass}`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                } else if (packageAdd === "n" || packageAdd === "N") {
-                  cmd.get(
-                    ` mkdir ${folderName} && cd ${folderName} && ${
-                      installCommands.cra
-                    } .`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                }
-              });
+              cmd.get(
+                ` mkdir ${folderName} && cd ${folderName} && ${
+                  installCommands.cra
+                } . && ${installCommands.reactRouter}`,
+                (err, data, stderr) => console.log(data)
+              );
             }
           });
-        } else if (folderName === ".") {
-          console.log("React Router");
+        } else if (packageAdd === "n" || packageAdd === "N") {
+          console.log("Node Sass");
           prompt(addPackage).then(({ packageAdd }) => {
             if (packageAdd === "y" || packageAdd === "Y") {
-              console.log("Node Sass");
-              prompt(addPackage).then(({ packageAdd }) => {
-                if (packageAdd === "y" || packageAdd === "Y") {
-                  cmd.get(
-                    ` ${installCommands.cra} . && ${
-                      installCommands.reactRouter
-                    } && ${installCommands.nodeSass}`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                } else if (packageAdd === "n" || packageAdd === "N") {
-                  cmd.get(
-                    ` ${installCommands.cra} . && ${
-                      installCommands.reactRouter
-                    }`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                }
-              });
+              cmd.get(
+                ` mkdir ${folderName} && cd ${folderName} && ${
+                  installCommands.cra
+                } . && ${installCommands.nodeSass}`,
+                (err, data, stderr) => console.log(data)
+              );
             } else if (packageAdd === "n" || packageAdd === "N") {
-              console.log("Node Sass");
-              prompt(addPackage).then(({ packageAdd }) => {
-                if (packageAdd === "y" || packageAdd === "Y") {
-                  cmd.get(
-                    ` ${installCommands.cra} . && ${installCommands.nodeSass}`,
-                    (err, data, stderr) => console.log(data)
-                  );
-                } else if (packageAdd === "n" || packageAdd === "N") {
-                  cmd.get(` ${installCommands.cra} .`, (err, data, stderr) =>
-                    console.log(data)
-                  );
-                }
-              });
+              cmd.get(
+                ` mkdir ${folderName} && cd ${folderName} && ${
+                  installCommands.cra
+                } .`,
+                (err, data, stderr) => console.log(data)
+              );
             }
           });
-        } else {
-          console.log(
-            "You must specify installation directory! (Enter a folder name, like 'my-app') "
-          );
         }
       });
+    } else if (folderName === ".") {
+      console.log("React Router");
+      prompt(addPackage).then(({ packageAdd }) => {
+        if (packageAdd === "y" || packageAdd === "Y") {
+          console.log("Node Sass");
+          prompt(addPackage).then(({ packageAdd }) => {
+            if (packageAdd === "y" || packageAdd === "Y") {
+              cmd.get(
+                ` ${installCommands.cra} . && ${
+                  installCommands.reactRouter
+                } && ${installCommands.nodeSass}`,
+                (err, data, stderr) => console.log(data)
+              );
+            } else if (packageAdd === "n" || packageAdd === "N") {
+              cmd.get(
+                ` ${installCommands.cra} . && ${installCommands.reactRouter}`,
+                (err, data, stderr) => console.log(data)
+              );
+            }
+          });
+        } else if (packageAdd === "n" || packageAdd === "N") {
+          console.log("Node Sass");
+          prompt(addPackage).then(({ packageAdd }) => {
+            if (packageAdd === "y" || packageAdd === "Y") {
+              cmd.get(
+                ` ${installCommands.cra} . && ${installCommands.nodeSass}`,
+                (err, data, stderr) => console.log(data)
+              );
+            } else if (packageAdd === "n" || packageAdd === "N") {
+              cmd.get(` ${installCommands.cra} .`, (err, data, stderr) =>
+                console.log(data)
+              );
+            }
+          });
+        }
+      });
+    } else {
+      console.log(
+        "You must specify installation directory! (Enter a folder name, like 'my-app') "
+      );
     }
   });
 };
