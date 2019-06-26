0374a3713233b3a54f20399d7a7de83d7be7acbd
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-06-26 11:11:00 +0530

    redux

--- cli routes/stateManagement.js
+++ cli routes/stateManagement.js
@@ -10,6 +10,8 @@ const stateOption = cliModel.stateManagement;
 // Cli Install Commands
 const { reduxObj } = require("../cli model/install-commands");
 
+const ReduxBoilerPlate = require("../cli model/starter-code/redux");
+
 const prompt = inquirer.createPromptModule();
 
 const stateManagement = () => {
@@ -23,46 +25,7 @@ const stateManagement = () => {
           });
           cmd.get(`cd store && touch store.js`);
           const writeStream = fs.createWriteStream("./store/store.js");
-          writeStream.write(`import { createStore } from "redux";
-
-const initialState = {
-  // Declare your state here
- };
-
- // Your reducer
- const reducer = (state = initialState, action) => {
-  //Use Switch statements
-   switch(action.type){
-     case '':
-     //You usually perform a state change and return it
-     default:
-       return state;
-   }
- }
-
- // Add the below code within these comment blocks,
- //to your respective files where you would like to have redux
-
- const mapStateToProps = state => {
-   return {
-     //Write your code here which connects the state of this component,
-     //to the Redux Store you have passed as props.
-   };
- };
-
- const mapDispatchToProps = dispatch => {
-   return {
-     //Write your code here which dispatches information to your reducer
-   };
- };
-// End
-
- // Create the store
- const store = createStore(reducer);
-
- //Exporting the Store
- export default store;
- `);
+          writeStream.write(`${ReduxBoilerPlate}`);
           console.log(
             "Packages: redux & react-redux has been installed successfully!"
           );
