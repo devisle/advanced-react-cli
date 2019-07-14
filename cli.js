90a014445489fe1eb65592daab4ade0ccdb3b358
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-14 11:29:15 +0530

    Refactored cli.js & code split to cliMainOptions.js

--- cli.js
+++ cli.js
@@ -15,37 +15,11 @@ const inquirer = require("inquirer");
 const cliModel = require("./cliModel");
 const cliCommand = cliModel.cliCommand;
 
-// Cli Routes
-const CRA = require("./cliRoutes/cra");
-const ReactRouter = require("./cliRoutes/reactrouter");
-const NodeSass = require("./cliRoutes/nodeSass");
-const ReactComponent = require("./cliRoutes/reactComponent");
-const PropTypes = require("./cliRoutes/propTypes");
-const StateManagement = require("./cliRoutes/stateManagement");
+// CLI Main Options
+const cliMainOptions = require("./cliMainOptions");
 
 const prompt = inquirer.createPromptModule();
 
 prompt(cliCommand).then(answers => {
-  switch (answers.installation) {
-    case "create-react-app":
-      new CRA().CRAPrompt();
-      break;
-    case "react-component":
-      new ReactComponent().whatComponent();
-      break;
-    case "react-router":
-      new ReactRouter().installOrUninstall();
-      break;
-    case "node-sass":
-      new NodeSass().installOrUninstall();
-      break;
-    case "prop-types":
-      new PropTypes().installOrUninstall();
-      break;
-    case "state-management":
-      new StateManagement().prompt();
-      break;
-    default:
-      console.log("You must select an option!");
-  }
+  cliMainOptions(answers);
 });
---
+++ cliMainOptions.js
@@ -0,0 +1,32 @@
+// Cli Routes
+const CRA = require("./cliRoutes/cra");
+const ReactRouter = require("./cliRoutes/reactrouter");
+const NodeSass = require("./cliRoutes/nodeSass");
+const ReactComponent = require("./cliRoutes/reactComponent");
+const PropTypes = require("./cliRoutes/propTypes");
+const StateManagement = require("./cliRoutes/stateManagement");
+
+module.exports = answers => {
+  switch (answers.installation) {
+    case "create-react-app":
+      new CRA().CRAPrompt();
+      break;
+    case "react-component":
+      new ReactComponent().whatComponent();
+      break;
+    case "react-router":
+      new ReactRouter().installOrUninstall();
+      break;
+    case "node-sass":
+      new NodeSass().installOrUninstall();
+      break;
+    case "prop-types":
+      new PropTypes().installOrUninstall();
+      break;
+    case "state-management":
+      new StateManagement().prompt();
+      break;
+    default:
+      console.log("You must select an option!");
+  }
+};
