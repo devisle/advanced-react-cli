7df8c866756cfffc4f3c0723f77ca923911cf2ff
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-15 20:26:00 +0530

    Updated install-commands NPM file

--- cliModel/install-commands.js
+++ cliModel/install-commands.js
@@ -1,32 +1,32 @@
 module.exports = {
   createReactApp: "npx create-react-app",
   propTypesObj: {
-    install: "npm i prop-types",
+    install: "npm i prop-types --save",
     uninstall: "npm uninstall prop-types"
   },
   reactRouterObj: {
-    install: "npm i react-router react-router-dom",
+    install: "npm i react-router react-router-dom --save",
     uninstall: "npm uninstall react-router react-router-dom"
   },
   nodeSassObj: {
-    install: "npm i node-sass",
+    install: "npm i node-sass --save",
     uninstall: "npm uninstall node-sass"
   },
   reduxObj: {
-    install: "npm i redux react-redux",
+    install: "npm i redux react-redux --save",
     uninstall: "npm uninstall redux react-redux"
   },
   reduxThunkObj: {
-    install: "npm i redux-thunk",
+    install: "npm i redux-thunk --save",
     uninstall: "npm uninstall redux-thunk"
   },
   unstatedObj: {
     unstated: {
-      install: "yarn add unstated",
-      uninstall: "yarn remove unstated"
+      install: "npm install unstated --save",
+      uninstall: "npm uninstall unstated"
     },
     unstatedNext: {
-      install: "npm install unstated-next",
+      install: "npm install unstated-next --save",
       uninstall: "npm uninstall unstated-next"
     }
   }
