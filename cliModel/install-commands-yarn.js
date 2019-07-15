14ac506658b683f6e65054e2eb51c75f4a656492
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-15 20:17:18 +0530

    Created new file with Yarn install commands

---
+++ cliModel/install-commands-yarn.js
@@ -0,0 +1,33 @@
+module.exports = {
+  createReactApp: "yarn create react-app my-app",
+  propTypesObj: {
+    install: "yarn add prop-types",
+    uninstall: "yarn remove prop-types"
+  },
+  reactRouterObj: {
+    install: "npm i react-router react-router-dom",
+    uninstall: "npm uninstall react-router react-router-dom"
+  },
+  nodeSassObj: {
+    install: "npm i node-sass",
+    uninstall: "npm uninstall node-sass"
+  },
+  reduxObj: {
+    install: "npm i redux react-redux",
+    uninstall: "npm uninstall redux react-redux"
+  },
+  reduxThunkObj: {
+    install: "npm i redux-thunk",
+    uninstall: "npm uninstall redux-thunk"
+  },
+  unstatedObj: {
+    unstated: {
+      install: "yarn add unstated",
+      uninstall: "yarn remove unstated"
+    },
+    unstatedNext: {
+      install: "npm install unstated-next",
+      uninstall: "npm uninstall unstated-next"
+    }
+  }
+};
