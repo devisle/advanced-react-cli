13ca8e6b81bca8731d3ed34532cdeeb7e472fc2f
Erno Salo <39559256+endormi@users.noreply.github.com>
2019-07-15 16:51:14 +0300

    Not a question

--- cliRoutes/cra.js
+++ cliRoutes/cra.js
@@ -23,7 +23,7 @@ module.exports = class CRA {
     prompt({
       ...installFolder[0],
       message:
-        "Please input the name of the folder you would like to create for your project? (Enter a folder name, e.g. 'my-app')"
+        "Please input the name of the folder you would like to create for your project. (Enter a folder name, e.g. 'my-app')"
     }).then(({ folderName }) => {
       if (folderName.length > 1) {
         /*
