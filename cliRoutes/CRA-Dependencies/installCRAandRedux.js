867b117c05dd76e4e36970878e5a3c80f4158d08
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-15 20:43:54 +0530

    Updated CRA install with NPM/Yarn package install choices

--- cliRoutes/CRA-Dependencies/installCRAandRedux.js
+++ cliRoutes/CRA-Dependencies/installCRAandRedux.js
@@ -4,17 +4,36 @@ const fs = require("fs");
 // Cli Install Commands
 const { createReactApp, reduxObj } = require("../../cliModel/install-commands");
 
+const {
+  createReactAppYarn,
+  reduxObjYarn
+} = require("../../cliModel/install-commands-yarn");
+
 // Import Redux Boilerplate
 const ReduxBoilerPlate = require("../../cliModel/starter-code/redux");
 
 module.exports = (folderName, packageInstaller) => {
-  fs.appendFile("store.js", ReduxBoilerPlate, err => {
-    if (err) throw err;
-  });
-  cmd.get(
-    ` mkdir ${folderName} && cd ${folderName} && ${createReactApp} . && ${
-      reduxObj.install
-    } && mkdir store && cd store && touch store.js && cat < ../../store.js > store.js && cd .. && cd .. && rm store.js `,
-    (err, data, stderr) => (err ? console.log(err) : console.log(stderr, data))
-  );
+  if (packageInstaller === "NPM") {
+    fs.appendFile("store.js", ReduxBoilerPlate, err => {
+      if (err) throw err;
+    });
+    cmd.get(
+      ` mkdir ${folderName} && cd ${folderName} && ${createReactApp} . && ${
+        reduxObj.install
+      } && mkdir store && cd store && touch store.js && cat < ../../store.js > store.js && cd .. && cd .. && rm store.js `,
+      (err, data, stderr) =>
+        err ? console.log(err) : console.log(stderr, data)
+    );
+  } else {
+    fs.appendFile("store.js", ReduxBoilerPlate, err => {
+      if (err) throw err;
+    });
+    cmd.get(
+      ` mkdir ${folderName} && cd ${folderName} && ${createReactAppYarn} . && ${
+        reduxObjYarn.install
+      } && mkdir store && cd store && touch store.js && cat < ../../store.js > store.js && cd .. && cd .. && rm store.js `,
+      (err, data, stderr) =>
+        err ? console.log(err) : console.log(stderr, data)
+    );
+  }
 };
