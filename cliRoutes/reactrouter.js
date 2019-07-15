fb403fcbc13bb4f95cab5f39588e05cd4aaf6e84
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-15 21:10:37 +0530

    Updated react-router  package with NPM/Yarn package install choices

--- cliRoutes/reactrouter.js
+++ cliRoutes/reactrouter.js
@@ -4,9 +4,12 @@ const cmd = require("node-cmd");
 // Cli Model
 const cliModel = require("../cliModel");
 const installOption = cliModel.installOption;
+const YarnOrNpm = cliModel.YarnOrNpm;
 
 // CLI Install Commands
 const { reactRouterObj } = require("../cliModel/install-commands");
+
+const { reactRouterObjYarn } = require("../cliModel/install-commands-yarn");
 
 const prompt = inquirer.createPromptModule();
 
@@ -17,30 +20,51 @@ const prompt = inquirer.createPromptModule();
 */
 module.exports = class ReactRouter {
   installOrUninstall() {
-    prompt(installOption).then(({ decision }) => {
-      if (decision === "Install") {
-        this.install();
-      } else if (decision === "Uninstall") {
-        this.uninstall();
-      }
+    prompt(YarnOrNpm).then(({ packageManager }) => {
+      const packageInstaller = packageManager;
+      prompt(installOption).then(({ decision }) => {
+        if (decision === "Install") {
+          this.install(packageInstaller);
+        } else if (decision === "Uninstall") {
+          this.uninstall(packageInstaller);
+        }
+      });
     });
   }
 
-  install() {
-    cmd.get(`${reactRouterObj.install}`, (err, data, stderr) => {
-      err ? console.log(err) : console.log(stderr, data);
-    });
-    console.log(
-      "Packages: react-router & react-router-dom has been installed successfully!"
-    );
+  install(packageInstaller) {
+    if (packageInstaller === "NPM") {
+      cmd.get(`${reactRouterObj.install}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Packages: react-router & react-router-dom has been installed successfully!"
+      );
+    } else {
+      cmd.get(`${reactRouterObjYarn.install}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Packages: react-router & react-router-dom has been installed successfully!"
+      );
+    }
   }
 
-  uninstall() {
-    cmd.get(`${reactRouterObj.uninstall}`, (err, data, stderr) => {
-      err ? console.log(err) : console.log(stderr, data);
-    });
-    console.log(
-      "Packages: react-router & react-router-dom has been uninstalled successfully!"
-    );
+  uninstall(packageInstaller) {
+    if (packageInstaller === "NPM") {
+      cmd.get(`${reactRouterObj.uninstall}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Packages: react-router & react-router-dom has been uninstalled successfully!"
+      );
+    } else {
+      cmd.get(`${reactRouterObjYarn.uninstall}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Packages: react-router & react-router-dom has been uninstalled successfully!"
+      );
+    }
   }
 };
