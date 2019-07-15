43edd214bd4c9a87ab3eecca6e6de0fa66ae1658
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-15 21:39:28 +0530

    Updated state-management packages with NPM/Yarn package install choices

--- cliRoutes/StateManagement-Dependencies/unstatedNext.js
+++ cliRoutes/StateManagement-Dependencies/unstatedNext.js
@@ -26,19 +26,36 @@ module.exports = class UnstatedNext {
   }
 
   install(packageInstaller) {
-    cmd.get(`${unstatedNext.install}`, (err, data, stderr) => {
-      err ? console.log(err) : console.log(stderr, data);
-    });
-    console.log(
-      "Check out more on how to get started with unstated-next on the following link https://github.com/jamiebuilds/unstated-next "
-    );
-    console.log("Package: Unstated-next has been installed!");
+    if (packageInstaller === "NPM") {
+      cmd.get(`${unstatedNext.install}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Check out more on how to get started with unstated-next on the following link https://github.com/jamiebuilds/unstated-next "
+      );
+      console.log("Package: Unstated-next has been installed!");
+    } else {
+      cmd.get(`${unstatedNextYarn.install}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log(
+        "Check out more on how to get started with unstated-next on the following link https://github.com/jamiebuilds/unstated-next "
+      );
+      console.log("Package: Unstated-next has been installed!");
+    }
   }
 
   uninstall(packageInstaller) {
-    cmd.get(`${unstatedNext.uninstall}`, (err, data, stderr) => {
-      err ? console.log(err) : console.log(stderr, data);
-    });
-    console.log("Package: Unstated-next has been uninstalled!");
+    if (packageInstaller === "NPM") {
+      cmd.get(`${unstatedNext.uninstall}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log("Package: Unstated-next has been uninstalled!");
+    } else {
+      cmd.get(`${unstatedNextYarn.uninstall}`, (err, data, stderr) => {
+        err ? console.log(err) : console.log(stderr, data);
+      });
+      console.log("Package: Unstated-next has been uninstalled!");
+    }
   }
 };
