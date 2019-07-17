ea733f8730a52c8df983e79aebf48251adb97c99
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-17 23:40:52 +0530

    added styled-components install commands

--- cliModel/install-commands.js
+++ cliModel/install-commands.js
@@ -1,34 +1,38 @@
 module.exports = {
-  createReactApp: 'npx create-react-app',
+  createReactApp: "npx create-react-app",
   propTypesObj: {
-    install: 'npm i prop-types --save',
-    uninstall: 'npm uninstall prop-types'
+    install: "npm i prop-types --save",
+    uninstall: "npm uninstall prop-types"
   },
   reactRouterObj: {
-    install: 'npm i react-router react-router-dom --save',
-    uninstall: 'npm uninstall react-router react-router-dom'
+    install: "npm i react-router react-router-dom --save",
+    uninstall: "npm uninstall react-router react-router-dom"
   },
   nodeSassObj: {
-    install: 'npm i node-sass --save',
-    uninstall: 'npm uninstall node-sass'
+    install: "npm i node-sass --save",
+    uninstall: "npm uninstall node-sass"
+  },
+  styledComponentsObj: {
+    install: "npm install styled-components",
+    uninstall: "npm uninstall styled-components"
   },
   reduxObj: {
-    install: 'npm i redux react-redux --save',
-    uninstall: 'npm uninstall redux react-redux'
+    install: "npm i redux react-redux --save",
+    uninstall: "npm uninstall redux react-redux"
   },
   reduxThunkObj: {
-    install: 'npm i redux-thunk --save',
-    uninstall: 'npm uninstall redux-thunk'
+    install: "npm i redux-thunk --save",
+    uninstall: "npm uninstall redux-thunk"
   },
   unstatedObj: {
     unstated: {
-      install: 'npm install unstated --save',
-      uninstall: 'npm uninstall unstated'
+      install: "npm install unstated --save",
+      uninstall: "npm uninstall unstated"
     },
     unstatedNext: {
-      install: 'npm install unstated-next --save',
-      uninstall: 'npm uninstall unstated-next'
+      install: "npm install unstated-next --save",
+      uninstall: "npm uninstall unstated-next"
     }
   }
   // easyPeasy: "npm install easy-peasy"
-}
+};
