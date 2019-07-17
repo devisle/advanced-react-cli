a47ebfb37420bf20b07afc94fa51d9bd65505b6f
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-17 18:31:32 +0530

    Added in error logging /w colors

--- cliRoutes/StateManagement/redux.js
+++ cliRoutes/StateManagement/redux.js
@@ -1,99 +1,100 @@
-const inquirer = require('inquirer')
-const fs = require('fs')
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const customCMD = require('../../customNodeCMD')
+const customCMD = require("../../customNodeCMD");
+const errorLogging = require("../../customNodeCMD/customError");
 
 // Cli Model
-const cliModel = require('../../cliModel/index')
-const installOption = cliModel.installOption
+const cliModel = require("../../cliModel/index");
+const installOption = cliModel.installOption;
 
 // Cli Install Commands
-const { reduxObj } = require('../../cliModel/install-commands')
+const { reduxObj } = require("../../cliModel/install-commands");
 
-const { reduxObjYarn } = require('../../cliModel/install-commands-yarn')
+const { reduxObjYarn } = require("../../cliModel/install-commands-yarn");
 
 // Importing Redux Boiler plate file
-const ReduxBoilerPlate = require('../../cliModel/starter-code/redux')
+const ReduxBoilerPlate = require("../../cliModel/starter-code/redux");
 
-const prompt = inquirer.createPromptModule()
+const prompt = inquirer.createPromptModule();
 
 module.exports = class Redux {
-  installOrUninstall (packageInstaller) {
+  installOrUninstall(packageInstaller) {
     prompt(installOption).then(({ decision }) => {
-      if (decision === 'Install') {
-        this.install(packageInstaller)
-      } else if (decision === 'Uninstall') {
-        this.uninstall(packageInstaller)
+      if (decision === "Install") {
+        this.install(packageInstaller);
+      } else if (decision === "Uninstall") {
+        this.uninstall(packageInstaller);
       }
-    })
+    });
   }
 
-  install (packageInstaller) {
-    fs.mkdir('./store', err => {
-      if (err) throw err
-    })
-    customCMD.run(`cd store && touch store.js`)
-    const writeStream = fs.createWriteStream('./store/store.js')
-    writeStream.write(`${ReduxBoilerPlate}`)
+  install(packageInstaller) {
+    fs.mkdir("./store", err => {
+      if (err) throw err;
+    });
+    customCMD.run(`cd store && touch store.js`);
+    const writeStream = fs.createWriteStream("./store/store.js");
+    writeStream.write(`${ReduxBoilerPlate}`);
 
     switch (packageInstaller) {
-      case 'NPM':
+      case "NPM":
         customCMD.get(
           `${reduxObj.install}`,
           (err, data, stderr) => {
-            err ? console.log(err) : console.log(stderr, data)
+            err ? console.log(err) : errorLogging(stderr, data);
           },
-          'install'
-        )
+          "install"
+        );
         console.log(
-          'Packages: redux & react-redux has been installed successfully!'
-        )
-        console.log('Redux Store has been created successfully!')
-        break
+          "Packages: redux & react-redux has been installed successfully!"
+        );
+        console.log("Redux Store has been created successfully!");
+        break;
 
-      case 'Yarn':
+      case "Yarn":
         customCMD.get(
           `${reduxObjYarn.install}`,
           (err, data, stderr) => {
-            err ? console.log(err) : console.log(stderr, data)
+            err ? console.log(err) : errorLogging(stderr, data);
           },
-          'install'
-        )
+          "install"
+        );
         console.log(
-          'Packages: redux & react-redux has been installed successfully!'
-        )
-        console.log('Redux Store has been created successfully!')
-        break
+          "Packages: redux & react-redux has been installed successfully!"
+        );
+        console.log("Redux Store has been created successfully!");
+        break;
     }
   }
 
-  uninstall (packageInstaller) {
+  uninstall(packageInstaller) {
     switch (packageInstaller) {
-      case 'NPM':
+      case "NPM":
         customCMD.get(
           `${reduxObj.uninstall}`,
           (err, data, stderr) => {
-            err ? console.log(err) : console.log(stderr, data)
+            err ? console.log(err) : errorLogging(stderr, data);
           },
-          'uninstall'
-        )
+          "uninstall"
+        );
         console.log(
-          'Packages: redux & react-redux has been uninstalled successfully!'
-        )
-        break
+          "Packages: redux & react-redux has been uninstalled successfully!"
+        );
+        break;
 
-      case 'Yarn':
+      case "Yarn":
         customCMD.get(
           `${reduxObjYarn.uninstall}`,
           (err, data, stderr) => {
-            err ? console.log(err) : console.log(stderr, data)
+            err ? console.log(err) : errorLogging(stderr, data);
           },
-          'uninstall'
-        )
+          "uninstall"
+        );
         console.log(
-          'Packages: redux & react-redux has been uninstalled successfully!'
-        )
-        break
+          "Packages: redux & react-redux has been uninstalled successfully!"
+        );
+        break;
     }
   }
-}
+};
