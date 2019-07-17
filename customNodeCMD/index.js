c5f90f82e6b3de7e934d4064e88dbfa221d1cd0d
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-17 18:24:00 +0530

    Added in error logging /w colors

--- customNodeCMD/index.js
+++ customNodeCMD/index.js
@@ -5,7 +5,6 @@
 */
 
 var exec = require("child_process").exec;
-const chalk = require("chalk");
 var CLI = require("clui"),
   Spinner = CLI.Spinner;
 
@@ -74,29 +73,4 @@ function getCommand(command, callback, installOrUninstall) {
   );
 }
 
-function errorLogging(stderr, data) {
-  const newData = data.data.split("\n");
-  const newStderr = stderr.stderr.split("\n");
-
-  newStderr.forEach(log => {
-    const warning = log.search("WARN");
-    if (warning) {
-      const warn = chalk.bgYellow.black("WARN");
-      log.replace("WARN", warn);
-      console.log(log.replace("WARN", warn));
-    }
-  });
-
-  newData.forEach(log => {
-    const warning = log.search("WARN");
-    if (warning) {
-      const warn = chalk.bgYellow("WARN");
-      log.replace("WARN", warn);
-      console.log(log.replace("WARN", warn));
-    }
-  });
-}
-
 module.exports = commandLineFunctions;
-
-module.exports = errorLogging;
