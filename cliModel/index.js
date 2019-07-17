c9ea68b05c8e37bbeab7fdc519d057aa85fadc99
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-17 23:58:27 +0530

    updated options

--- cliModel/index.js
+++ cliModel/index.js
@@ -1,99 +1,101 @@
 module.exports.cliCommand = [
   {
-    type: 'list',
-    name: 'installation',
-    message: 'What would you like to install?',
+    type: "list",
+    name: "installation",
+    message: "What would you like to install?",
     choices: [
-      'create-react-app',
-      'react-component',
-      'react-router',
-      'node-sass',
-      'prop-types',
-      'state-management'
+      "CreateReactApp",
+      "ReactComponent",
+      "ReactRouter",
+      "NodeSass",
+      "Styled Components",
+      "PropTypes",
+      "StateManagement",
+      "TypeScript"
     ]
   }
-]
+];
 
 module.exports.installOption = [
   {
-    type: 'list',
-    name: 'decision',
-    message: 'Would you like to Install or Uninstall?',
-    choices: ['Install', 'Uninstall']
+    type: "list",
+    name: "decision",
+    message: "Would you like to Install or Uninstall?",
+    choices: ["Install", "Uninstall"]
   }
-]
+];
 
 // Add new package Yes/No
 
 module.exports.addPackage = [
   {
-    type: 'input',
-    name: 'packageAdd',
+    type: "input",
+    name: "packageAdd",
     message: `Would you like to add a package? (Y/N)`
   }
-]
+];
 
 // Input name for Install Folder
 
 module.exports.installFolder = [
   {
-    type: 'input',
-    name: 'folderName',
+    type: "input",
+    name: "folderName",
     message:
-      'Please input the name of the folder you would like to create for your project? (Enter a . if for current directory)'
+      "Please input the name of the folder you would like to create for your project? (Enter a . if for current directory)"
   }
-]
+];
 
 // Input name when creating new components for React project
 
 module.exports.componentName = [
   {
-    type: 'input',
-    name: 'componentName',
+    type: "input",
+    name: "componentName",
     message:
-      'Please enter the file name of the component you would like to create: '
+      "Please enter the file name of the component you would like to create: "
   }
-]
+];
 
 // React Components selection: Functional/Class
 
 module.exports.reactComponents = [
   {
-    type: 'list',
-    name: 'component',
-    message: 'Function or Class Component?',
-    choices: ['function', 'class']
+    type: "list",
+    name: "component",
+    message: "Function or Class Component?",
+    choices: ["function", "class"]
   }
-]
+];
 
 // React State Management tools: More options soon! ('Unstated', Easy-Peasy')
 module.exports.stateManagement = [
   {
-    type: 'list',
-    name: 'state',
+    type: "list",
+    name: "state",
     message:
-      'Please select the tool to add to your project for state management: ',
-    choices: ['Redux', 'Unstated', 'Redux-Thunk']
+      "Please select the tool to add to your project for state management: ",
+    choices: ["Redux", "Unstated", "Redux-Thunk"]
   }
-]
+];
 
 // Unstated options
 module.exports.unstatedOptions = [
   {
-    type: 'list',
-    name: 'state',
+    type: "list",
+    name: "state",
     message:
-      'Please select the which version of Unstated you would like to add to your project: ',
-    choices: ['Unstated', 'Unstated-next']
+      "Please select the which version of Unstated you would like to add to your project: ",
+    choices: ["Unstated", "Unstated-next"]
   }
-]
+];
 
 // Package Manager Options
 module.exports.YarnOrNpm = [
   {
-    type: 'list',
-    name: 'packageManager',
-    message: 'Would you like to install using Yarn or NPM?',
-    choices: ['Yarn', 'NPM']
+    type: "list",
+    name: "packageManager",
+    message: "Would you like to install using Yarn or NPM?",
+    choices: ["Yarn", "NPM"]
   }
-]
+];
