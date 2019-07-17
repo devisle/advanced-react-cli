1b6e458811d19ac3f1d0cdc6d0acd363da408271
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-17 23:55:35 +0530

    Added in styled-components route

--- cliMainOptions.js
+++ cliMainOptions.js
@@ -1,32 +1,36 @@
 // Cli Routes
-const CRA = require('./cliRoutes/CRA/cra')
-const ReactRouter = require('./cliRoutes/reactrouter')
-const NodeSass = require('./cliRoutes/nodeSass')
-const ReactComponent = require('./cliRoutes/ReactComponent/mainReactComponent')
-const PropTypes = require('./cliRoutes/propTypes')
-const StateManagement = require('./cliRoutes/StateManagement/stateManagement')
+const CRA = require("./cliRoutes/CRA/cra");
+const ReactRouter = require("./cliRoutes/reactrouter");
+const NodeSass = require("./cliRoutes/nodeSass");
+const StyledComponents = require("./cliRoutes/styledComponents");
+const ReactComponent = require("./cliRoutes/ReactComponent/mainReactComponent");
+const PropTypes = require("./cliRoutes/propTypes");
+const StateManagement = require("./cliRoutes/StateManagement/stateManagement");
 
 module.exports = answers => {
   switch (answers.installation) {
-    case 'create-react-app':
-      new CRA().CRAPrompt()
-      break
-    case 'react-component':
-      new ReactComponent().componentPrompt()
-      break
-    case 'react-router':
-      new ReactRouter().installOrUninstall()
-      break
-    case 'node-sass':
-      new NodeSass().installOrUninstall()
-      break
-    case 'prop-types':
-      new PropTypes().installOrUninstall()
-      break
-    case 'state-management':
-      new StateManagement().prompt()
-      break
+    case "create-react-app":
+      new CRA().CRAPrompt();
+      break;
+    case "react-component":
+      new ReactComponent().componentPrompt();
+      break;
+    case "react-router":
+      new ReactRouter().installOrUninstall();
+      break;
+    case "node-sass":
+      new NodeSass().installOrUninstall();
+      break;
+    case "styled-components":
+      new StyledComponents().installOrUninstall();
+      break;
+    case "prop-types":
+      new PropTypes().installOrUninstall();
+      break;
+    case "state-management":
+      new StateManagement().prompt();
+      break;
     default:
-      console.log('You must select an option!')
+      console.log("You must select an option!");
   }
-}
+};
