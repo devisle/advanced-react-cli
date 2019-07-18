cbc4039b359432288c13cef860ce1ea224b42244
Shreyas Balachandran <shreyas.bba@gmail.com>
2019-07-18 10:09:58 +0530

    Folder rearrangement

---
+++ cliRoutes/StylingPackages/nodeSass.js
@@ -0,0 +1,89 @@
+const inquirer = require("inquirer");
+
+const customCMD = require("../../customNodeCMD");
+const errorLogging = require("../../customNodeCMD/customError");
+
+// Cli Model
+const cliModel = require("../../cliModel");
+const installOption = cliModel.installOption;
+const YarnOrNpm = cliModel.YarnOrNpm;
+
+// CLI Install Commands
+const { nodeSassObj } = require("../../cliModel/install-commands");
+
+const { nodeSassObjYarn } = require("../../cliModel/install-commands-yarn");
+
+const prompt = inquirer.createPromptModule();
+
+/*
+
+  Installs/Uninstalls Node-Sass package
+
+*/
+
+module.exports = class NodeSass {
+  installOrUninstall() {
+    prompt(YarnOrNpm).then(({ packageManager }) => {
+      const packageInstaller = packageManager;
+      prompt(installOption).then(({ decision }) => {
+        if (decision === "Install") {
+          this.install(packageInstaller);
+        } else if (decision === "Uninstall") {
+          this.uninstall(packageInstaller);
+        }
+      });
+    });
+  }
+
+  install(packageInstaller) {
+    switch (packageInstaller) {
+      case "NPM":
+        customCMD.get(
+          `${nodeSassObj.install}`,
+          (err, data, stderr) => {
+            err ? console.log(err) : errorLogging(stderr, data);
+          },
+          "install"
+        );
+        console.log("Package: node-sass has been installed successfully!");
+        break;
+
+      case "Yarn":
+        customCMD.get(
+          `${nodeSassObjYarn.install}`,
+          (err, data, stderr) => {
+            err ? console.log(err) : errorLogging(stderr, data);
+          },
+          "install"
+        );
+        console.log("Package: node-sass has been installed successfully!");
+        break;
+    }
+  }
+
+  uninstall(packageInstaller) {
+    switch (packageInstaller) {
+      case "NPM":
+        customCMD.get(
+          `${nodeSassObj.uninstall}`,
+          (err, data, stderr) => {
+            err ? console.log(err) : errorLogging(stderr, data);
+          },
+          "uninstall"
+        );
+        console.log("Package: node-sass has been uninstalled successfully!");
+        break;
+
+      case "Yarn":
+        customCMD.get(
+          `${nodeSassObjYarn.uninstall}`,
+          (err, data, stderr) => {
+            err ? console.log(err) : errorLogging(stderr, data);
+          },
+          "uninstall"
+        );
+        console.log("Package: node-sass has been uninstalled successfully!");
+        break;
+    }
+  }
+};
