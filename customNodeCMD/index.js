bc62f67ce1a62ab164cf8388267341af37af7aa2
shreyas <shreyas.bba@gmail.com>
2020-01-10 22:25:26 +0530

    Integrated Bek's Progress Bar Changes to CustomCMD

--- customNodeCMD/index.js
+++ customNodeCMD/index.js
@@ -4,100 +4,94 @@
  Kindly visit https://github.com/RIAEvangelist/node-cmd and support them <3
 */
 
+const spawn = require('child_process').spawn
 const exec = require('child_process').exec
-const ora = require('ora')
-const cliProgress = require('cli-progress')
+// const ora = require('ora');
 
 const commandLineFunctions = {
   get: getCommand,
   run: runCommand
 }
 
-const install = new ora({
-  text: 'Installing package(s)...',
-  install: process.argv[2],
-  indent: 1,
-  spinner: {
-    interval: 80,
-    frames: [
-      '( ●    )',
-      '(  ●   )',
-      '(   ●  )',
-      '(    ● )',
-      '(     ●)',
-      '(    ● )',
-      '(   ●  )',
-      '(  ●   )',
-      '( ●    )',
-      '(●     )'
-    ]
-  }
-})
+// const install = new ora({
+//   text: 'Installing package(s)...',
+//   install: process.argv[2]
+// }) //Posible deletion
 
-const uninstall = new ora({
-  text: 'Uninstalling package(s)...',
-  uninstall: process.argv[2],
-  spinner: {
-    interval: 80,
-    frames: [
-      '( ●    )',
-      '(  ●   )',
-      '(   ●  )',
-      '(    ● )',
-      '(     ●)',
-      '(    ● )',
-      '(   ●  )',
-      '(  ●   )',
-      '( ●    )',
-      '(●     )'
-    ]
-  }
-})
+// const uninstall = new ora({
+//   text: 'Uninstalling package(s)...',
+//   uninstall: process.argv[2]
+// }) //Possible deletion
 
 function runCommand (command) {
   return exec(command)
 }
 
-function getCommand (command, callback, installOrUninstall) {
-  exec(
-    command,
-    (function () {
-      switch (installOrUninstall) {
-        case 'install':
-          install.start()
-          // console.log(install.start())
-          return function (err, data, stderr) {
-            if (!callback) {
-              return
-            }
-            setTimeout(() => {
-              install.color = 'green'
-            }, 3000)
-            process.stdout.write('\n')
-            callback(err, { data }, { stderr })
-            process.exit()
-            install.succeed()
-          }
-          break
+/*
+This feature was aided by my friend Gensen, github: choyg.
+I just want to give him credit for helping me with node related question.
+Todo: Change the node default progress bar to something different.
+Display custom message instead default node progress bar
+*/
 
-        case 'uninstall':
-          uninstall.start()
-          return function (err, data, stderr) {
-            if (!callback) {
-              return
-            }
-            setTimeout(() => {
-              uninstall.color = 'green'
-            }, 3000)
-            // callback(err, { data }, { stderr });
-            process.stdout.write('\n')
-            callback(err, { data }, { stderr })
-            process.exit()
-            uninstall.succeed()
-          }
-      }
-    })(callback)
-  )
+function getCommand (command, packages) {
+  console.log('Installing Packages...')
+  const subproc = spawn(command, {
+    stdio: 'inherit',
+    shell: true
+  })
+  if (subproc.stdout !== null) {
+    subproc.stdout.on('data', data => {
+      console.log(`Creating project: ${data}...`)
+    })
+    subproc.stderr.on('data', data => {
+      console.error(`stderr: ${data}`)
+    })
+  }
+
+  return
+  // exec(
+  //   //Why exec and code below is greyed out?
+  //   command,
+  //   (function() {
+  //     switch (installOrUninstall) {
+  //       case "install":
+  //         // install.start()
+
+  //         // console.log(install.start())
+  //         return function(err, data, stderr) {
+  //           if (!callback) {
+  //             return;
+  //           }
+  //           setTimeout(() => {
+  //             install.color = "green";
+  //           }, 3000);
+  //           process.stdout.write("\n");
+  //           callback(err, { data }, { stderr });
+  //           // console.log('\x1b[33m%s\x1b[0m', data);  //yellow
+
+  //           process.exit();
+  //           install.succeed();
+  //         };
+  //         break;
+
+  //       case "uninstall":
+  //         uninstall.start();
+  //         return function(err, data, stderr) {
+  //           if (!callback) {
+  //             return;
+  //           }
+  //           setTimeout(() => {
+  //             uninstall.color = "green";
+  //           }, 3000);
+  //           // callback(err, { data }, { stderr });
+  //           process.stdout.write("\n");
+  //           callback(err, { data }, { stderr });
+  //           process.exit();
+  //           uninstall.succeed();
+  //         };
+  //     }
+  //   })(callback)
+  // );
 }
-
 module.exports = commandLineFunctions
